25ab88ba1aafdbe87cdf6bd20dff7418e8ed3182
Dongwan Kim <dongwan.don.kim@gmail.com>
2022-01-03 23:16:50 -0800

    feat: user class

--- dist/src.f10117fe.js
+++ dist/src.f10117fe.js
@@ -117,9 +117,74 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"src/index.ts":[function(require,module,exports) {
-console.log('hi');
-},{}],"../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+})({"src/models/User.ts":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.User = void 0;
+
+var User =
+/** @class */
+function () {
+  function User(data) {
+    this.data = data;
+    this.events = {};
+  }
+
+  User.prototype.get = function (propName) {
+    return this.data[propName];
+  };
+
+  User.prototype.set = function (update) {
+    Object.assign(this.data, update);
+  };
+
+  User.prototype.on = function (eventName, callback) {
+    var handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
+  };
+
+  User.prototype.trigger = function (eventName) {
+    var handlers = this.events[eventName];
+    if (!handlers || handlers.length === 0) return;
+    handlers.forEach(function (callback) {
+      callback();
+    });
+  };
+
+  return User;
+}();
+
+exports.User = User;
+},{}],"src/index.ts":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var User_1 = require("./models/User");
+
+var user = new User_1.User({
+  name: 'donkim',
+  age: 33
+});
+user.on('change', function () {
+  console.log('aef');
+});
+user.on('change', function () {
+  console.log('ijojiojio');
+});
+user.on('hi', function () {
+  console.log('hihi');
+});
+user.trigger('change');
+user.trigger('hi');
+console.log(user);
+},{"./models/User":"src/models/User.ts"}],"../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -147,7 +212,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60377" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62168" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('hi');
+import { User } from './models/User';
+
+const user = new User({ name: 'donkim', age: 33 });
+
+user.on('change', () => {
+  console.log('aef');
+});
+user.on('change', () => {
+  console.log('ijojiojio');
+});
+user.on('hi', () => {
+  console.log('hihi');
+});
+user.trigger('change');
+user.trigger('hi');
+console.log(user);
