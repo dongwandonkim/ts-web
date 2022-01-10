d6f61f1b4068ce6b76e2c3cf10959dc02cf22a33
Dongwan Kim <dongwan.don.kim@gmail.com>
2022-01-10 02:52:30 -0800

    User inherits Model

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,12 @@ import { User } from './models/User';
 
 // axios.post('http://localhost:3000/users', { name: 'dondon', age: 12 });
 
-const user = new User({ name: 'test kim', age: 33 });
+// const user = new User({ name: 'kimkim', age: 99 });
 
-console.log(user.get('name'));
+// console.log(user.get('name'));
+
+// user.on('save', () => {
+//   console.log(user);
+// });
+
+// user.save();
