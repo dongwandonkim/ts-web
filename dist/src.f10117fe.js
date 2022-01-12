92a0770a45560666f58161cf8d8e8c6b709c538a
Dongwan Kim <dongwan.don.kim@gmail.com>
2022-01-11 20:38:16 -0800

    extracted a View class

--- dist/src.f10117fe.js
+++ dist/src.f10117fe.js
@@ -140,6 +140,18 @@ function () {
       _this.model.setRandomAge();
     };
 
+    this.onSetName = function () {
+      var input = _this.parent.querySelector('input');
+
+      if (input) {
+        var name = input.value;
+
+        _this.model.set({
+          name: name
+        });
+      }
+    };
+
     this.bindModel();
   }
 
@@ -153,20 +165,13 @@ function () {
 
   UserForm.prototype.eventsMap = function () {
     return {
-      'click:.set-age': this.onSetAgeClick
+      'click:.set-age': this.onSetAgeClick,
+      'click:.set-name': this.onSetName
     };
   };
 
-  UserForm.prototype.onHeaderHover = function () {
-    console.log('h1 hover');
-  };
-
-  UserForm.prototype.onButtonClick = function () {
-    console.log('update name');
-  };
-
   UserForm.prototype.template = function () {
-    return "\n      <div>\n        <h1>User Form</h1>\n        <div>User name: " + this.model.get('name') + "</div>\n        <div>User age: " + this.model.get('age') + "</div>\n        <input />\n        <button class=\"update-name\">Update Name</button>\n        <button class=\"set-age\">Set Random Age</button>\n      </div>\n    ";
+    return "\n      <div>\n        <h1>User Form</h1>\n        <div>User name: " + this.model.get('name') + "</div>\n        <div>User age: " + this.model.get('age') + "</div>\n        <input />\n        <button class=\"set-name\">Update Name</button>\n        <button class=\"set-age\">Set Random Age</button>\n      </div>\n    ";
   };
 
   UserForm.prototype.bindEvents = function (fragment) {
@@ -2605,8 +2610,14 @@ var user = User_1.User.buildUser({
   name: 'NAME',
   age: 20
 });
-var userForm = new UserForm_1.UserForm(document.getElementById('root'), user);
-userForm.render();
+var root = document.getElementById('root');
+
+if (root) {
+  var userForm = new UserForm_1.UserForm(root, user);
+  userForm.render();
+} else {
+  throw new Error('root element not found');
+}
 },{"./views/UserForm":"src/views/UserForm.ts","./models/User":"src/models/User.ts"}],"../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
--- src/views/Views.ts
+++ src/views/Views.ts
@@ -1,45 +1,17 @@
 import { User } from '../models/User';
 
-export class UserForm {
+export abstract class View {
   constructor(public parent: Element, public model: User) {
     this.bindModel();
   }
+
+  abstract eventsMap(): { [key: string]: () => void };
+  abstract template(): string;
 
   bindModel(): void {
     this.model.on('change', () => {
       this.render();
     });
-  }
-  eventsMap(): { [key: string]: () => void } {
-    return {
-      'click:.set-age': this.onSetAgeClick,
-      'click:.set-name': this.onSetName,
-    };
-  }
-  onSetAgeClick = (): void => {
-    console.log('age change');
-    this.model.setRandomAge();
-  };
-
-  onSetName = (): void => {
-    const input = this.parent.querySelector('input');
-    if (input) {
-      const name = input.value;
-      this.model.set({ name });
-    }
-  };
-
-  template(): string {
-    return `
-      <div>
-        <h1>User Form</h1>
-        <div>User name: ${this.model.get('name')}</div>
-        <div>User age: ${this.model.get('age')}</div>
-        <input />
-        <button class="set-name">Update Name</button>
-        <button class="set-age">Set Random Age</button>
-      </div>
-    `;
   }
 
   bindEvents(fragment: DocumentFragment): void {
